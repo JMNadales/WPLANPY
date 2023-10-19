8cdd88bf72b1a781ef4806698fd4ae25fd663459
Juan Moreno Nadales <64591295+JMNadales@users.noreply.github.com>
2023-10-19 12:41:58 +0200

    Update scheduling_example.py

--- scheduling_example.py
+++ scheduling_example.py
@@ -15,12 +15,12 @@ import matplotlib.pyplot as plt
                             # CONNECTION TO MONGODB
 ###############################################################################
 # Connection uri
-mongo_uri = "mongodb+srv://nadales:1234@cluster0.991bqjq.mongodb.net/"
+mongo_uri = "YOU DATA BASE URI"
 
 # Connect to DB
 client = pymongo.MongoClient(mongo_uri)
 # Access DB
-db = client.sample_scheduling
+db = client."YOUR DATA BASE"
 
 ###############################################################################
                             # READ WATERWAY DATA
@@ -40,8 +40,8 @@ speed_limits_inv = [1/x for x in speed_limits]
                             # READ VESSELS DATA
 ###############################################################################
 # Select waterway data collection
-collection_vessels_up   = db.vessels_chipiona
-collection_vessels_down = db.vessels_seville
+collection_vessels_up   = db."YOUR ORIGIN PORT"
+collection_vessels_down = db."YOUR DESTINY PORT"
 
 # Vessels beams
 beam_up   = []
@@ -91,32 +91,18 @@ critic_points = [0, 491, 870]
 #####################################################################################################                                        
 [plan, tubes_up, tubes_down, n_tubes_up, n_tubes_down] = wplanpy.planning(n_up, n_down, time_ready_up, time_ready_down, beam_up, beam_down, vmax_up, vmax_down,draft_up, draft_down, time_dead_up, time_dead_down, critic_points,dist, width, speed_limits, speed_limits_inv, waypoints)
 
-# Ajustar el tamaño de la fuente de Matplotlib solo para los números en los ejes
-plt.rc('xtick', labelsize=16)  # Tamaño de fuente de los números en el eje x
-plt.rc('ytick', labelsize=16)  # Tamaño de fuente de los números en el eje y
 
-# Crear una figura con fondo blanco
+plt.rc('xtick', labelsize=16) 
+plt.rc('ytick', labelsize=16) 
+
+
 plt.figure(figsize=(10, 6), tight_layout=True, facecolor='white')
-
-# Establecer el fondo del gráfico en blanco
-#plt.gca().set_facecolor('white')
-# Establecer el tamaño de la fuente y la familia de fuente
 plt.rcParams['font.size'] = 20
 plt.rcParams["font.family"] = "serif"
-
-
-# Crear un gráfico de líneas
 for i in range(0, n_up + n_down):
     plt.plot(waypoints, plan[i, 0:len(waypoints)], 'o--', linewidth=1)
-
-# Establecer el fondo del gráfico en blanco
 plt.gca().set_facecolor('white')
-
-# Agregar cuadrícula al gráfico
 plt.grid(True)
-
 plt.xlabel('Position (km/10)', fontsize=16)
 plt.ylabel('Time (h/10)', fontsize=16)
-
-# Mostrar el gráfico en pantalla
 plt.show()
